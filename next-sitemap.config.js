b8e4c4d4b04490c22dbda141682bc83563f2b831
Francisco <fjvc2891@users.noreply.github.com>
2026-02-08 22:06:04 -0500

    Add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://vciurislexsas.com.co',  // sin www, como usas ahora
+  generateRobotsTxt: true,                   // genera robots.txt automáticamente
+  // No necesitas estas líneas si no aplican:
+  // sitemapSize: 7000,      // solo si tienes miles de páginas
+  // changefreq: 'weekly',
+  // priority: 0.7,
+  // exclude: ['/secret-page*'],  // bórrala o coméntala
+};
